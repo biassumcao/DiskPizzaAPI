e3ec25ea20b1d976d04b9d9df008d6cf63c8be28
Beatriz Assumção da Silva <b.assumcao.da.silva@accenture.com>
2023-11-09 20:23:58 -0300

    setup environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'password',
-        database: 'database',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PASSWORD),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
         synchronize: true,
       }),
     }),
