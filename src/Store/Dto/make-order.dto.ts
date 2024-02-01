ed5d3efefb8e902aa4ce28f780e1113b9211a518
Beatriz Assumção da Silva <b.assumcao.da.silva@accenture.com>
2024-02-01 16:02:17 -0300

    feat: validate dto properties

--- src/Store/Dto/make-order.dto.ts
+++ src/Store/Dto/make-order.dto.ts
@@ -1,23 +1,30 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsNumber, IsString } from 'class-validator';
 
 export class Pizzas {
+  @IsString()
   @ApiProperty()
   flavor: string;
 
+  @IsNumber()
   @ApiProperty()
   amount: number;
 }
 
 export class MakeOrderDto {
+  @IsArray()
   @ApiProperty({ type: [Pizzas] })
   pizzas: Pizzas[];
 
+  @IsArray()
   @ApiProperty({ isArray: true, type: 'number' })
   pizzasAmount: number[];
 
+  @IsString()
   @ApiProperty()
   address: string;
 
+  @IsString()
   @ApiProperty()
   paymentMethod: string;
 }
